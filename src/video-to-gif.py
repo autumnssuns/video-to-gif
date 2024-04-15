7daa9697c9cc70c72c146eb63ecd4c2247fc733b
Dan Tran <d30.tran@qut.edu.au>
2024-04-15 19:55:54 +1000

    Added optimisation and set target frame rate to 10

--- src/video-to-gif.py
+++ src/video-to-gif.py
@@ -1,6 +1,7 @@
-import imageio
+import imageio.v3 as iio
 import os, sys
 import numpy as np
+from pygifsicle import optimize
 
 class TargetFormat(object):
     GIF = ".gif"
@@ -12,22 +13,26 @@ def convertFile(input_path, targetFormat):
     output_path = os.path.splitext(input_path)[0] + targetFormat
     print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(input_path, output_path))
 
-    reader = imageio.get_reader(input_path)
-    fps = reader.get_meta_data()['fps']
-    duration = reader.get_meta_data()['duration']
+    data = iio.imread(input_path)
+    
+    metadata = iio.immeta(input_path)
+    fps = metadata.get('fps', 30)
+    target_fps = 10
+    duration = metadata.get('duration', len(data)/fps)
+    
     print(f"fps: {fps}, duration: {duration}")
 
     frames = []
     FADE_DURATION = 0.75
     # Fade in the first frame
-    first_frame = reader.get_data(0)
+    first_frame = data[0]
     for i in range(int(fps*FADE_DURATION)):
         # Fade from black
         faded_frame = first_frame * 0.9 * (i/(fps*FADE_DURATION))
         faded_frame = faded_frame.astype(np.uint8)
         frames.append(faded_frame)
     
-    for i, im in enumerate(reader):
+    for i, im in enumerate(data):
         sys.stdout.write("\rFrame {0}".format(i))
         sys.stdout.flush()
         frames.append(im)
@@ -40,8 +45,14 @@ def convertFile(input_path, targetFormat):
         faded_frame = faded_frame.astype(np.uint8)
         frames.append(faded_frame)
     
+    # Skip frames to reduce the size of the gif
+    skip_frames = int(fps/target_fps)
+    frames = frames[::skip_frames]
+    
     print("\r\nWriting...")
-    imageio.mimsave(output_path, frames, format='GIF', fps=fps, loop=0)
+    iio.imwrite(output_path, frames, format='GIF', duration=duration, fps=target_fps, loop=0)
+    print("\r\nOptimizing...")
+    optimize(output_path)
 
     # writer = imageio.get_writer(output_path, fps=fps, format='GIF')
     # for i,im in enumerate(reader):
